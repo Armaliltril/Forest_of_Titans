7781bfbb6ed397482874b8e659be7450ce7c926a
Armaliltril <armaliltril@gmail.com>
2018-05-24 20:46:13 +0300

    Made Tree levels Eatable

--- src/main/kotlin/com/company/experimental/tree/TreeFood.kt
+++ src/main/kotlin/com/company/experimental/tree/TreeFood.kt
@@ -1,11 +1,14 @@
 package com.company.experimental.tree
 
+import com.company.experimental.Eatable
+import com.company.experimental.animal.AnimalType
 import com.company.forest.Forest
 import java.lang.Integer.min
 
 data class TreeFood(var onCrown: Int, var onTrunk: Int, var onRoots: Int,
                     var maxOnCrown: Int, var maxOnTrunk: Int, var maxOnRoots: Int) {
-    enum class Level {
+
+    enum class Level: Eatable {
         CROWN {
             override fun getSeasonCoef() = when(Forest.currentWeather) {
                 Forest.Weather.SUNNY -> 2
@@ -19,6 +22,8 @@ data class TreeFood(var onCrown: Int, var onTrunk: Int, var onRoots: Int,
                 Forest.Season.SUMMER -> 4
                 Forest.Season.AUTUMN -> 2
             }
+
+            override val eatableBy = arrayListOf(AnimalType.WOODPECKER, AnimalType.FLYING_SQUIRREL)
         },
         TRUMP {
             override fun getSeasonCoef() = when(Forest.currentWeather) {
@@ -33,6 +38,7 @@ data class TreeFood(var onCrown: Int, var onTrunk: Int, var onRoots: Int,
                 Forest.Season.SUMMER -> 2
                 Forest.Season.AUTUMN -> 3
             }
+            override var eatableBy = arrayListOf(AnimalType.WOODPECKER)
         },
         ROOTS {
             override fun getSeasonCoef() = when(Forest.currentWeather) {
@@ -47,6 +53,7 @@ data class TreeFood(var onCrown: Int, var onTrunk: Int, var onRoots: Int,
                 Forest.Season.SUMMER -> 3
                 Forest.Season.AUTUMN -> 4
             }
+            override var eatableBy = arrayListOf(AnimalType.BADGER, AnimalType.CHIPMUNK, AnimalType.SQUIRREL)
         };
 
         abstract fun getWeatherCoef(): Int
